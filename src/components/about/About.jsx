59b49cac902fd4af1462f7e83619399a466fee32
Stephan DuVal <stephan.duval@gmail.com>
2022-06-23 17:03:33 -0400

    end of day commit - halfway done the about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import "./about.css";
+import { GiIonicColumn } from "react-icons/gi";
+import { FaUsers } from "react-icons/fa";
+import { BiHomeSmile } from "react-icons/bi";
+import StephanDuValBabyPhoto from "../../assets/stephanduval-throwingbaby.jpg";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="about__me__image">
+        <img src={StephanDuValBabyPhoto} alt="About Image" />
+      </div>
+      <div className="about__content"></div>
+      <div className="about__cards">
+        <article className="about__card">
+          <GiIonicColumn className="about__icon" />
+          <h5>Experience</h5>
+          <small>2+ Years As a Developer</small>
+          <small>14+ Years running a profitable web business.</small>
+        </article>
+        <article className="about__card">
+          <FaUsers className="about__icon" />
+          <h5>Clients</h5>
+          <small>2+ Clients</small>
+          <small>100+ Usersj.</small>
+        </article>
+        <article className="about__card">
+          <BiHomeSmile className="about__icon" />
+          <h5>Projects</h5>
+          <small>6 Completed Projects</small>
+          <small>3 Currently in daily use</small>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default About;
