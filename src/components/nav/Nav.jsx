3aabfaed281cd5e3bd38e816f712bbd8f4546bc5
Stephan DuVal <stephan.duval@gmail.com>
2022-06-23 16:23:09 -0400

    added github calendar to porfolio page

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./nav.css";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillTelephoneFill } from "react-icons/bs";
@@ -8,22 +9,44 @@ import { GiMountainClimbing } from "react-icons/gi";
 import { IconContext } from "react-icons";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
       <IconContext.Provider value={{ color: "#4db5ff" }}>
-        <a href="#">
+        <a
+          href="#"
+          onClick={() => setActiveNav("#")}
+          className={activeNav === "#" ? "active" : ""}
+        >
           <AiFillHome />
         </a>
-        <a href="#about">
+        <a
+          href="#about"
+          onClick={() => setActiveNav("about")}
+          className={activeNav === "about" ? "active" : ""}
+        >
           <FcManager />
         </a>
-        <a href="#experience">
+        <a
+          href="#experience"
+          onClick={() => setActiveNav("experience")}
+          className={activeNav === "experience" ? "active" : ""}
+        >
           <GiMountainClimbing />
         </a>
-        <a href="#portfolio">
+        <a
+          href="#portfolio"
+          onClick={() => setActiveNav("portfolio")}
+          className={activeNav === "portfolio" ? "active" : ""}
+        >
           <ImEmbed2 />
         </a>
-        <a href="#contact">
+        <a
+          href="#contact"
+          onClick={() => setActiveNav("contact")}
+          className={activeNav === "contact" ? "active" : ""}
+        >
           <BsFillTelephoneFill />
         </a>
       </IconContext.Provider>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./portfolio.css";
+import GitHubCalendar from "react-github-calendar";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      Portfolio
+      <GitHubCalendar username="stephanduval" blockRadius="15" />
+    </section>
+  );
 };
 
 export default Portfolio;
