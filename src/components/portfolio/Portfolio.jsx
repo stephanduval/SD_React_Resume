39656c85ccd6ac03b5b96b4546f435b2eb9ddbab
Stephan DuVal <stephan.duval@gmail.com>
2022-06-27 15:44:32 -0400

    Fixed Nav Icons for vercel deployment

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "./portfolio.css";
 import GitHubCalendar from "react-github-calendar";
+import NBACK from "../../assets/Portfolio/n-Back.png";
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+      <GitHubCalendar username="stephanduval" blockRadius="15" />
+
       <div className="container portfolio__container">
         <article className="portfolio__item">
-          <div> className="portfolio__item-image"</div>
-          <h3>This is a portfolio title</h3>
+          <div className="portfolio__item-image">
+            <img
+              src={NBACK}
+              alt="annes Hepner Cognitive Reappraisal and n-Back Experiment"
+            ></img>
+          </div>
+          <h3>Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment</h3>
+          <a href="https://github.com/stephanduval" target="blank">
+            Github
+          </a>
         </article>
       </div>
-      <GitHubCalendar username="stephanduval" blockRadius="15" />
     </section>
   );
 };
