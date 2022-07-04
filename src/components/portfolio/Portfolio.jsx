534f683a618a22c903eb683d202021d2b1f05418
Stephan DuVal <stephan.duval@gmail.com>
2022-07-04 13:56:56 -0700

    created a map for portfolio items

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,27 @@ import "./portfolio.css";
 // import GitHubCalendar from "react-github-calendar";
 import NBACK from "../../assets/Portfolio/n-Back.png";
 
+const portfolioData = [
+  {
+    id: 1,
+    image: NBACK,
+    title: "Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment",
+    github: "https://github.com/stephanduval",
+    demo: "https://dashboard.heroku.com/apps/n-back-experiment",
+    description:
+      "An interactable real world study that uses Hooks and Redux to track users as they respond to cognitive tests ",
+  },
+  {
+    id: 2,
+    image: NBACK,
+    title: "Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment",
+    github: "https://github.com/stephanduval",
+    demo: "https://dashboard.heroku.com/apps/n-back-experiment",
+    description:
+      "An interactable real world study that uses Hooks and Redux to track users as they respond to cognitive tests ",
+  },
+];
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -11,73 +32,30 @@ const Portfolio = () => {
       {/* <GitHubCalendar username="stephanduval" blockRadius="15" /> */}
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src={NBACK}
-              alt="annes Hepner Cognitive Reappraisal and n-Back Experiment"
-            ></img>
-          </div>
-          <h3>Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment</h3>
-          <a href="https://github.com/stephanduval" target="blank">
-            Github
-          </a>
-          <a
-            href="https://dashboard.heroku.com/apps/n-back-experiment"
-            className="btn btn-primary"
-            target="blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src={NBACK}
-              alt="annes Hepner Cognitive Reappraisal and n-Back Experiment"
-            ></img>
-          </div>
-          <h3>Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment</h3>
-          <a href="https://github.com/stephanduval" target="blank">
-            Github
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src={NBACK}
-              alt="annes Hepner Cognitive Reappraisal and n-Back Experiment"
-            ></img>
-          </div>
-          <h3>Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment</h3>
-          <a href="https://github.com/stephanduval" target="blank">
-            Github
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src={NBACK}
-              alt="annes Hepner Cognitive Reappraisal and n-Back Experiment"
-            ></img>
-          </div>
-          <h3>Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment</h3>
-          <a href="https://github.com/stephanduval" target="blank">
-            Github
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img
-              src={NBACK}
-              alt="annes Hepner Cognitive Reappraisal and n-Back Experiment"
-            ></img>
-          </div>
-          <h3>Hannes Hepner Cognitive Reappraisal & N-Back ExpeNriment</h3>
-          <a href="https://github.com/stephanduval" target="blank">
-            Github
-          </a>
-        </article>
+        {portfolioData.map(
+          ({ id, image, title, github, demo, description }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title}></img>
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-links">
+                  <div className="portfolio__GitHub-link">
+                    <a href={github} target="blank">
+                      Github
+                    </a>
+                  </div>
+                  <div className="portfolio_item-cta">
+                    <a href={demo} className="btn btn-primary" target="blank">
+                      Live Demo
+                    </a>
+                  </div>
+                </div>
+              </article>
+            );
+          }
+        )}
       </div>
     </section>
   );
