ee8fb00ccddaeb0eb28e9cabeaa2029d65ea2e34
Stephan DuVal <stephan.duval@gmail.com>
2022-07-11 11:37:14 -0400

    small change to experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,8 +4,11 @@ import "./experience.css";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>Skills</h5>
-      <div className="section__title">My Experience</div>
+      <div className="experience__container">
+        <h5>Skills</h5>
+        <div className="section__title">My Experience</div>
+        <h6> 12 Years of experience in web commerce.</h6>
+      </div>
     </section>
   );
 };
